16e56541476bd780e0c3390f7c711b20f77500ae
Kalandoros <129214458+Kalandoros@users.noreply.github.com>
2023-09-13 19:58:40 +0000

    x

--- src/berechnungen.py
+++ src/berechnungen.py
@@ -1,1 +1,13 @@
+def stosskurzschlussstrom(x):
+    """
+    Funktion zur Berechnung des Stosskurzschlussstromes
+    """
+    i_p = x+x
+    return i_p
 
+def thermisch_gleichwertiger_kurzschlussstrom(x):
+    """
+    Funktion zur Berechnung des thermisch gleichwertigen kurzschlussstromes
+    """
+    i_th = x^2
+    return i_th
