db112caec03f52d397b6c5b3d0c347a9839c985f
Kalandoros <angelo.rusvai@hotmail.com>
2023-10-09 22:10:40 +0200

    Erstellung von Funktion für Stosskurzschlussstrom und κ-Faktors zur Berechnung des Stosskurzschlussstromes

--- src/berechnungen.py
+++ src/berechnungen.py
@@ -1,13 +1,34 @@
-def stosskurzschlussstrom(x):
+import math
+
+
+def κ_faktor(r_x: float, r: float, x: float) -> float:
     """
-    Funktion zur Berechnung des Stosskurzschlussstromes
+    Funktion zur Berechnung κ-Faktors zur Berechnung des Stosskurzschlussstromes (dimensionslos)
+    r_x: Verhältnis R/X (dimensionslos)
     """
-    i_p = x+x
+    κ = 1.02 + 0.98 * math.exp(r_x)
+    return κ
+
+
+def stosskurzschlussstrom(i_k3: float, κ: float) -> float:
+    """
+    Funktion zur Berechnung des Stosskurzschlussstromes ip in A
+    i_k3: dreipoliger Anfangs-Kurzschlusswechselstrom (Effektivwert) in A
+    κ: Faktor zur Berechnung des Stosskurzschlussstromes
+    """
+    i_p = κ * math.sqrt(2) * i_k3
     return i_p
 
-def thermisch_gleichwertiger_kurzschlussstrom(x):
+
+def thermisch_gleichwertiger_kurzschlussstrom(i_k3: float, m: float, n: float) -> float:
     """
     Funktion zur Berechnung des thermisch gleichwertigen kurzschlussstromes
+
     """
-    i_th = x^2
-    return i_th
+    pass
+    # i_th = math.sqrt((math.)) ^ 2
+    # return i_th
+
+
+if __name__ == "__main__":
+    print(stosskurzschlussstrom(5000, 1.25))
